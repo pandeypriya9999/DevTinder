3a9d88406d7bb0250b0328222dca82e7dc1c03a2
Priya Pandey <priyapandey@Priyas-iMac.local>
2025-02-23 07:17:38 +0530

    forgot password api created

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const profileRouter = express.Router();
+const bcrypt = require("bcrypt");
 const User = require('../models/user');
 const logger = require("../utils/logger");
 const authUser = require("../middlewares/auth");
 
 //to fetch user profile based JWT token
-profileRouter.get("/profile", authUser, async (req, res) => {
+profileRouter.get("/profile/view", authUser, async (req, res) => {
 
   try {
     const user = req.user;
@@ -13,6 +14,69 @@ profileRouter.get("/profile", authUser, async (req, res) => {
   } catch (error) {
     logger.info(error);
     res.send(error.message);
+  }
+});
+
+//to update existed profile data
+profileRouter.patch("/profile/edit", authUser, async (req, res) => {
+  const userId = req.user?._id;
+  const data = req.body;
+
+  try {
+    const UPDATES_ALLOWED = [
+      "photoUrl",
+      "about",
+      "age",
+      "skills",
+      "gender"
+    ]
+
+    // Data sanitizing  started
+    const isAllowed = Object.keys(data).every((k) =>
+      UPDATES_ALLOWED.includes(k)
+    );
+
+    if (!isAllowed) {
+      throw new Error("User Update is not allowed");
+    }
+
+    if (data.skills.length > 10) {
+      throw new Error("Skills cannot be added more than 10");
+    }
+    // Data sanitizing  ended
+
+    const updatedResponse = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: 'after',
+      runValidators: true
+    });
+    res.json({
+      message: `${req.user.firstName} your profile got updated successfully`,
+      data: updatedResponse
+    });
+  } catch (error) {
+    res.send(error.message)
+  }
+});
+
+//to update or create new password after forgot
+profileRouter.patch("/profile/password", authUser, async (req, res) => {
+  try {
+    const newPasswordbyUser = req.body.password;
+    const user = req.user;
+
+    const isPasswordUnique = await user.passwordValidated(newPasswordbyUser);
+    const hashNewPassword = await bcrypt.hash(newPasswordbyUser, 10);
+    if (isPasswordUnique) {
+      throw new Error("Password cannot be same as current one");
+    } else {
+      await User.findByIdAndUpdate({ _id: user._id }, { password: hashNewPassword }, {
+        returnDocument: 'after',
+        runValidators: true
+      });
+      res.send(`${user.firstName} your password got updated successfully`);
+    }
+  } catch (error) {
+    res.send(error.message)
   }
 });
 
